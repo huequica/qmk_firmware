f9e89aff49a112e6f50fca7e2197f049a18a43cc
huequica <harumaki1225@gmail.com>
2020-08-03 11:13:40 +0900

    Fキーなんて必要ねえんだよ(暴論)

--- keyboards/uzu42/keymaps/huequica/keymap.c
+++ keyboards/uzu42/keymaps/huequica/keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_ADJUST] = LAYOUT( \
   //,---------------------------------------.                  ,---------------------------------------.
-       KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                     KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10,\
+       KC_F1,  KC_F2,  KC_F3,  KC_F4,KC_LPRN,                   KC_RPRN,  KC_F7,  KC_F8,  KC_F9, KC_F10,\
   //|-------+-------+-------+-------+-------|                  |-------+-------+-------+-------+-------|
       KC_F11, KC_F12,  RESET,KC_PSCR, KC_INS,                   RGB_TOG,RGB_HUI,RGB_SAI,RGB_VAI,XXXXXXX,\
   //|-------+-------+-------+-------+-------|                  |-------+-------+-------+-------+-------|
