6180104457ab757e69b50e5eb834bb38d42f2655
huequica <harumaki1225@gmail.com>
2020-08-07 00:31:18 +0900

    shiftとCommand入れかえ

--- keyboards/uzu42/keymaps/huequica/keymap.c
+++ keyboards/uzu42/keymaps/huequica/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|-------+-------+-------+-------+-------|                  |----------+-------+-------+-------+-------|
         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                      KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,\
   //|-------+-------+-------+-------+-------+--------|  |-------+---------+-------+-------+-------+-------|
-     KC_CTPL, KC_SPC,KC_LALT,KC_LSFT, KC_LGUI, KC_BSPC,   KC_ENT, SPCE_LOWR,KC_RALT,KC_RCTL,KC_F11, KC_F12 \
+     KC_CTPL, KC_SPC,KC_LALT,KC_LGUI,KC_LSFT, KC_BSPC,   KC_ENT, SPCE_LOWR,KC_RALT,KC_RCTL,KC_F11, KC_F12 \
   //|-------+-------+-------+-------+-------+--------|  |-------+---------+-------+-------+-------+-------|
   ),
 
